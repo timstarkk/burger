ffdb53cc6ab2b8b9e1ad8d486853593457ad1a9a
timstarkk <timstarkk@icloud.com>
2019-12-30 20:31:53 -0600

    created controller with routes

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const burger = require('../models/burger.js')
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.redirect("/burgers");
+});
+
+router.get('/burgers', (req, res) => {
+    burger.all(burgerData => {
+        res.render("index", { burger_data: burgerData });
+    });
+})
+
+
+router.get('/burgers/create', (req, res) => {
+    burger.create(req.body.burger_name, function (result) {
+        console.log(result);
+        res.redirect("/");
+    });
+});
+
+
+router.put("/burgers/:id", (req, res) => {
+    burger.update(req.params.id, function (result) {
+        console.log(result);
+
+        res.sendStatus(200);
+    });
+});
