255478e2df34875ff29665c154f03331d66f91d5
timstarkk <timstarkk@icloud.com>
2019-12-30 20:34:35 -0600

    db folder creation with schema and seeds for sql db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Alamo', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Greenbelt', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Liquid Gold', false);
